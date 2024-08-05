76aa178142696fbd18c30afdf682370a5c591289
galibmulani <mulanigalib1@gmail.com>
2024-08-05 12:55:11 +0530

    folder restructuring

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -44,7 +44,7 @@ class ModelTrainer:
                 test_array[:,:-1],
                 test_array[:,-1]
             )
-
+            #model
             models = {"Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
@@ -53,7 +53,7 @@ class ModelTrainer:
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
                 }
-            
+            #hyper parameters 
             params = {
                 "Decision Tree": {
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
--- src/utils.py
+++ src/utils.py
@@ -6,13 +6,13 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import r2_score
 from src.exception import CustomException
 
-def save_object(filepath,obj):
+def save_object(file_path,obj):
     try:
-        dir_path=os.path.dirname(filepath)
+        dir_path=os.path.dirname(file_path)
 
         os.makedirs(dir_path,exist_ok=True)
 
-        with open(filepath,"wb") as file_obj:
+        with open(file_path,"wb") as file_obj:
             pickle.dump(obj,file_obj)
     except Exception as e:
         raise CustomException(e,sys)
@@ -42,5 +42,13 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]]=test_model_score
         
         return report;
+
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
